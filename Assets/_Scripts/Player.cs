376a333bf5baf876789980e9e716a4cef6be01c9
krethick <krethick@gmail.com>
2024-03-26 23:24:38 +0530

    Reference and value types completed

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     //  horizontalAxis = "Horizontal" Left and Right values, verticalAxis = "Vertical"; // Up and Down values;
+    // private Rigidbody2D _rb2d = new Rigidbody2D(); // This is a constructor
     [SerializeField]
     private string _horizontalAxis = "Horizontal",  _verticalAxis = "Vertical"; 
     [SerializeField]
-    private Rigidbody2D _rb2d;
+    private Rigidbody2D _rb2d;  // This is a constructor
     
     private Vector2 _input; // Declare the input
+
+    [SerializeField]
+    private float _speed = 3f;
     private void FixedUpdate() {
-        _rb2d.velocity = _input; // After that assign _input to _rb2d.velocity 
+        _rb2d.velocity = _input * _speed; // After that assign _input to _rb2d.velocity 
     }
 
     /*
@@ -37,5 +41,6 @@ public class Player : MonoBehaviour
         float horizontalInput = Input.GetAxisRaw(_horizontalAxis); // Left and Right
         float VerticalInput = Input.GetAxisRaw(_verticalAxis); // Up and Down
         _input = new Vector2(horizontalInput, VerticalInput); // Store the above two in the variable _input
+        _input.Normalize(); // Fixes the magnitude and makes it 1.
     }
 }
